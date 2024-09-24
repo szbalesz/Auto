c6b393a909e208723d6f8f87627e472254b697ce
SzBalesz <szarazb@kkszki.hu>
2024-09-24 12:47:38 +0200

    Connect class is created

---
+++ Connect.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    public class Connect
+    {
+        public static MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "auto";
+            Username = "root";
+            Password = "";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
